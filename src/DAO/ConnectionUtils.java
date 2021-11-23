6d665dbc75f62151fb8ff7e9981aba231c1c51ae
CaoHoaiTan <caohoaitan@gmail.com>
2021-11-23 08:57:11 +0700

    Create Project

---
+++ src/DAO/ConnectionUtils.java
@@ -0,0 +1,41 @@
+package DAO;
+ 
+import java.sql.Connection;
+import java.sql.SQLException;
+ 
+public class ConnectionUtils {
+ 
+    public static Connection getConnection() 
+              throws ClassNotFoundException, SQLException {
+    	// get connection
+       // return SQLServerConnUtils.getSQLServerConnection();
+    	return MySQLConnUtils.getMySQLConnection();
+    }
+     
+    public static void closeQuietly(Connection conn) {
+        try {
+            conn.close();
+        } catch (Exception e) {
+        }
+    }
+ 
+    public static void rollbackQuietly(Connection conn) {
+        try {
+            conn.rollback();
+        } catch (Exception e) {
+        }
+    }
+    // Test connection
+    public static void main(String[] args) {
+    	try {
+			new ConnectionUtils();
+			System.out.print(ConnectionUtils.getConnection());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
