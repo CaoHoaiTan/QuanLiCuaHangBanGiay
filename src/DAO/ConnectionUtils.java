5bdb5f195bde62c7cb3f4d62c8c36f2814ff8926
Nam <thanhnam.thai01@gmail.com>
2021-11-23 17:32:36 +0700

    Thêm SQL Script Thêm form main, form quản lý khách hàng COnvert sang Maven project

--- src/DAO/ConnectionUtils.java
+++ src/DAO/ConnectionUtils.java
@@ -9,22 +9,22 @@ public class ConnectionUtils {
               throws ClassNotFoundException, SQLException {
     	// get connection
         return SQLServerConnUtils.getSQLServerConnection();
-//    	return MySQLConnUtils.getMySQLConnection();
     }
-     
+
     public static void closeQuietly(Connection conn) {
         try {
             conn.close();
         } catch (Exception e) {
         }
     }
- 
+
     public static void rollbackQuietly(Connection conn) {
         try {
             conn.rollback();
         } catch (Exception e) {
         }
     }
+
     // Test connection
     public static void main(String[] args) {
     	try {
---
+++ src/GUI/QuanLyKhachHang.java
@@ -0,0 +1,157 @@
+package GUI;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import javax.swing.plaf.InternalFrameUI;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+import java.awt.*;
+
+public class QuanLyKhachHang extends JInternalFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField txtPhoneNumber;
+	private JTextField txtFullName;
+	private JTextField txtAddress;
+	private JTextField txtEmail;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					QuanLyKhachHang frame = new QuanLyKhachHang();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * 
+	 * @throws UnsupportedLookAndFeelException
+	 */
+	public QuanLyKhachHang() throws UnsupportedLookAndFeelException {
+		setFrameIcon(null);
+		setBorder(null);
+		UIManager.setLookAndFeel(new FlatLightLaf());
+
+		setBounds(100, 100, 891, 706);
+		getContentPane().setLayout(new MigLayout("", "[grow]", "[][grow][]"));
+
+		JPanel input_panel = new JPanel();
+		getContentPane().add(input_panel, "cell 0 0,grow");
+		input_panel.setLayout(new MigLayout("", "[][][][][][grow]", "[24px][24px][24px][24px][24px][24px]"));
+
+		JLabel lblNewLabel = new JLabel("Số điện thoại:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel, "cell 0 0,alignx left,aligny center");
+
+		txtPhoneNumber = new JTextField();
+		txtPhoneNumber.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(txtPhoneNumber, "cell 1 0 5 1,growx,aligny top");
+		txtPhoneNumber.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("Họ và tên:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel_1, "cell 0 1,alignx left,aligny center");
+
+		txtFullName = new JTextField();
+		txtFullName.setFont(new Font("Tahoma", Font.BOLD, 16));
+		txtFullName.setColumns(10);
+		input_panel.add(txtFullName, "cell 1 1 5 1,growx,aligny top");
+
+		JLabel lblNewLabel_2 = new JLabel("Giới tính:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel_2, "cell 0 2,alignx left,aligny center");
+
+		JRadioButton rdbtnMale = new JRadioButton("Nam");
+		rdbtnMale.setFont(new Font("Tahoma", Font.BOLD, 14));
+		input_panel.add(rdbtnMale, "cell 1 2,alignx left,aligny center");
+
+		JLabel lblNewLabel_3 = new JLabel("Địa chỉ:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel_3, "cell 0 3,alignx left,aligny center");
+
+		txtAddress = new JTextField();
+		txtAddress.setFont(new Font("Tahoma", Font.BOLD, 16));
+		txtAddress.setColumns(10);
+		input_panel.add(txtAddress, "cell 1 3 5 1,growx,aligny top");
+
+		JLabel lblNewLabel_4 = new JLabel("Email:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel_4, "cell 0 4,alignx left,aligny center");
+
+		txtEmail = new JTextField();
+		txtEmail.setFont(new Font("Tahoma", Font.BOLD, 16));
+		txtEmail.setColumns(10);
+		input_panel.add(txtEmail, "cell 1 4 5 1,growx,aligny top");
+
+		JLabel lblNewLabel_5 = new JLabel("Ưu đãi 0%");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel_5, "cell 0 5,alignx left,aligny center");
+
+		JRadioButton rdbtnFemale = new JRadioButton("Nữ");
+		rdbtnFemale.setFont(new Font("Tahoma", Font.BOLD, 14));
+		input_panel.add(rdbtnFemale, "cell 3 2,alignx left,aligny center");
+
+		JRadioButton rdbtnOthers = new JRadioButton("Khác");
+		rdbtnOthers.setFont(new Font("Tahoma", Font.BOLD, 14));
+		input_panel.add(rdbtnOthers, "cell 5 2,alignx left,aligny center");
+
+		JLabel lblNewLabel_5_1 = new JLabel("Tổng số tiền đã mua: vnđ");
+		lblNewLabel_5_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		input_panel.add(lblNewLabel_5_1, "cell 5 5,growx,aligny center");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, "cell 0 1,grow");
+
+		table = new JTable();
+		table.setFont(new Font("Tahoma", Font.BOLD, 16));
+		scrollPane.setViewportView(table);
+
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, "cell 0 2,grow");
+		panel.setLayout(new MigLayout("", "[79px][63px][63px][63px][63px]", "[29px]"));
+
+		JButton btnThm = new JButton("Thêm");
+		btnThm.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel.add(btnThm, "cell 0 0,alignx left,aligny top");
+
+		JButton btnSa = new JButton("Sửa");
+		btnSa.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel.add(btnSa, "cell 1 0,alignx left,aligny top");
+
+		JButton btnXa = new JButton("Xóa");
+		btnXa.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel.add(btnXa, "cell 2 0,alignx left,aligny top");
+
+		JButton btnLu = new JButton("Lưu");
+		btnLu.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel.add(btnLu, "cell 3 0,alignx left,aligny top");
+
+		JButton btnNewButton = new JButton("Hủy");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel.add(btnNewButton, "cell 4 0,alignx left,aligny top");
+
+	}
+
+	// Tắt title bar của JInternalFrame
+	@Override
+	public void setUI(InternalFrameUI ui) {
+		super.setUI(ui);
+		BasicInternalFrameUI frameUI = (BasicInternalFrameUI) getUI();
+		if (frameUI != null)
+			frameUI.setNorthPane(null);
+	}
+}
